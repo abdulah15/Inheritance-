63bb35ee398075192a02f5a4c0782591ba809265
Abdullah <abdullahrehan2010@gmail.com>
2024-12-27 14:49:43 +0500

    Add files via upload

---
+++ p.py
@@ -0,0 +1,13 @@
+class student:
+definit_(self, name, grade) :
+self.name=name self-grade=grade
+def display(self):
+    print( "student decails:",self.name,self.grade)
+class Teacher (student):
+    def _init_(self, schoolname, name, grade):
+        self.schoolname=schoolname 
+        student._init_(name, grade)
+def display(selt):
+    print("student details:", self.name, self grade, self.schoolname)
+obj1=student ("mohammad",8, "psvw")
+obj1.display()  
